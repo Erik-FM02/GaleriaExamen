65510b5bb3faebd9896ff921eb2441b931d0c77a
Erik <erikmora2002@hotmail.com>
2023-11-08 17:06:20 +0100

    inicio paquete galeria

---
+++ src/main/java/edu/cesur/GaleriaArte/GaleriaExamenApplication.java
@@ -0,0 +1,13 @@
+package edu.cesur.GaleriaArte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleriaExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cesur/GaleriaArte/GaleriaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cesur.GaleriaArte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
